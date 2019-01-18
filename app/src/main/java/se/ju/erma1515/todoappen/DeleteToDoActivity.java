3c423448aa4f437f2b547af47ff34ffda408a50e
MarkusEson <markus.eson@hotmail.com>
2019-01-18 09:31:28 +0100

    changing delete function

--- app/src/main/java/se/ju/erma1515/todoappen/DeleteToDoActivity.java
+++ app/src/main/java/se/ju/erma1515/todoappen/DeleteToDoActivity.java
@@ -1,10 +1,14 @@
 package se.ju.erma1515.todoappen;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.view.View;
 
 public class DeleteToDoActivity extends AppCompatActivity {
 
@@ -14,5 +18,31 @@ public class DeleteToDoActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_delete);
+    }
+
+    public void onDeleteButtonClicked(View view) {
+        System.out.println("DELETE THIS TODO");
+
+        Integer position = getIntent().getIntExtra("todoIndex", -1);
+        Data.ToDo todoToDelete = Data.todos.get(position);
+
+        new AlertDialog.Builder(this).setTitle("Delete this ToDo?").setMessage("Do you really want to delete this ToDo?")
+                .setPositiveButton(
+                        android.R.string.yes,
+                        new DialogInterface().OnClickListener(){
+                            public void onClick(DialogInterface dialog, int whichButton) {
+                                // delete it
+                            }
+                        }
+                ).setNegativebutton(
+                        android.R.string.no,
+                        new DialogInterface.OnClickListener(){
+                            public void onClick(DialogInterface dialog, int whichButton) {
+                                // do not delete it
+                            }
+                        }
+                ).show();
+
+
     }
 }
